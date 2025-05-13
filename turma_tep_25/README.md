d10b260d779a13ea7c8763b50e528267dc8d4145
Gustavo Chagas do Nascimento <gustavochagas816@gmail.com>
2025-05-13 20:25:06 -0300

    Delete turma_tep_25 directory

--- turma_tep_25/README.md
+++
@@ -1,2 +0,0 @@
-# turma_tep_25
-Projeto Responsável pelas entregas da turma de tópicos para programação 2025.1 - Uniesp Faculdades
--- turma_tep_25/aqr_com_erro.py
+++
@@ -1,5 +0,0 @@
-nome = input("digite seu nome aqui: ")
-idade = int(input("Digite a sua idade: "))
-
-print(f"olá, {nome}")
-print(f"você tem {idade} anos!")
